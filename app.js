97ff6b6c46f1b804f011206b78d72c82c1b32162
JoKord <jtpcordeiro@gmail.com>
2016-11-28 11:05:23 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,28 @@
+'use strict'
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// uncomment after placing your favicon in /public
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const index = require('./routes/index');
+app.use('/', index);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+module.exports = app;
---
+++ mocha_tests/tests.js
@@ -0,0 +1,8 @@
+'use strict'
+let assert = require('assert');
+let supertest = require('supertest');
+let request = supertest.agent("http://localhost:3000");
+
+describe('Routes', function () {
+	
+});
