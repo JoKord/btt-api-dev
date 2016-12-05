7cf46de6f7df3e5ffdf556082669baba355d720d
JoKord <jtpcordeiro@gmail.com>
2016-12-05 12:10:38 +0000

    Started Client Development

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,32 @@
 
 module.exports = function(grunt) {
 
+  grunt.initConfig({
+      bower: {
+          dev: {
+              base: 'bower_components', /* the path to the bower_components directory */
+              dest: 'web/bower_components',
+              options: {
+                  checkExistence: true,
+                  debugging: true,
+                  paths: {
+                      bowerDirectory: 'bower_components',
+                      bowerrc: '.bowerrc',
+                      bowerJson: 'bower.json'
+                  }
+              }
+          },
+          flat: { /* flat folder/file structure */
+              dest: 'public/vendor',
+              options: {
+                  debugging: true
+              }
+          }
+      }
+  });
+
+  grunt.loadNpmTasks('main-bower-files');
+
   // Default task.
-  grunt.registerTask('default', []);
+  grunt.registerTask('default', ['bower']);
 };
